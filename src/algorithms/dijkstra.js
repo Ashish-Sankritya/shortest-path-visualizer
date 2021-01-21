36f056e132ac335b662acc1da51102ef4c334d71
Ashish-Sankritya <33031179+Ashish-Sankritya@users.noreply.github.com>
2021-01-21 13:37:29 +0530

    Update dijkstra.js
    Fix !! redundancy

--- src/algorithms/dijkstra.js
+++ src/algorithms/dijkstra.js
@@ -6,7 +6,7 @@ export function dijkstra(grid, startNode, finishNode) {
   const visitedNodesInOrder = [];
   startNode.distance = 0;
   const unvisitedNodes = getAllNodes(grid);
-  while (!!unvisitedNodes.length) {
+  while (unvisitedNodes.length) {
     sortNodesByDistance(unvisitedNodes);
     const closestNode = unvisitedNodes.shift();
     // If we encounter a wall, we skip it.
